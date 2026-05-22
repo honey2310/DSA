4d1d03314eda183a01a379dfd1f5029c3261f044
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2026-05-22 16:18:49 +0530

    Update Q1.js

--- DSA-PR/Q1.js
+++ DSA-PR/Q1.js
@@ -1,5 +1,4 @@
 let array = [1, 2, 2, 3, 4, 5, 5];
-// let array = [1, 2, 3, 4, 5, 5];
 
 for (let i = 0; i < array.length - 1; i++) { // 
   if (array[i] === array[i + 1]) { // array[1] == array[2]
